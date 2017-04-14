98273107ee43c9a56591312a9f6d4aa3cc943896
Bangjie Liu <jacob.liu16@gmail.com>
2017-04-13 22:36:36 -0400

    updated run.sh

--- src/SASCacheSim_DirBased/run.sh
+++ src/SASCacheSim_DirBased/run.sh
@@ -1,7 +1,7 @@
 cd ../App && make clean && make
 cd ../SASCacheSim_DirBased
-echo 'export PIN_ROOT=/afs/cs.cmu.edu/academic/class/15740-s17/public/pin-3.0'
 
+PIN_ROOT=/afs/cs.cmu.edu/academic/class/15740-s17/public/pin-3.0
 make clean
 make
 $PIN_ROOT/pin -t obj-intel64/SASCache.so -- ../App/app
